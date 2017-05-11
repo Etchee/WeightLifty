404fc6f43815e26f597ca7ccceb2ad0f085ead37
rikutoechigoya <rikutoechigoya@fuji.waseda.jp>
2017-05-11 23:26:25 +0900

    Color values changed

--- app/src/main/java/etchee/com/weightlifty/Activity/SettingsActivity.java
+++ app/src/main/java/etchee/com/weightlifty/Activity/SettingsActivity.java
@@ -5,6 +5,9 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.annotation.XmlRes;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import etchee.com.weightlifty.R;
 
@@ -18,4 +21,11 @@ public class SettingsActivity extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
 }
